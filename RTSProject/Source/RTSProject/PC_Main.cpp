877f7fe970d2b3cc3dc624cd33675aa63997ab14
CatherineSzobelDAE <55833598+CatherineSzobel@users.noreply.github.com>
2022-12-29 23:18:29 +0100

    Created first formation
    the first alternative line formation

--- RTSProject/Source/RTSProject/PC_Main.cpp
+++ RTSProject/Source/RTSProject/PC_Main.cpp
@@ -2,4 +2,74 @@
 
 
 #include "PC_Main.h"
+#include "DrawDebugHelpers.h"
+void APC_Main::CallFormation()
+{
+	for (FVector& unitPosition : FormationVector)
+	{
+		DrawDebugCircle(GetWorld(), unitPosition, 10.f, 12, FColor::Black, false, 20.f);
+	}
+}
+
+void APC_Main::MakeFormation(FVector rightClick, TArray<AActor*> units)
+{
+	if (rightClick == FVector(0.f))
+	{
+		FormationVector.AddUnique(units[0]->GetActorLocation());
+		NewPosition = units[0]->GetActorLocation();
+	}
+	else
+	{
+		FormationVector.AddUnique(rightClick);
+		NewPosition = rightClick;
+	}
+	switch (formationType)
+	{
+	case StraightLineVerticalRight:
+		GEngine->AddOnScreenDebugMessage(1, 10.f, FColor::Black, "1");
+		for (AActor* unit : units)
+		{
+			CreateFormationArray(LineRight());
+			GEngine->AddOnScreenDebugMessage(1, 10.f, FColor::Black, "1");
+		}
+		break;
+	case StraightLineVerticalLeft:
+		ArrayInput = LineLeft();
+		break;
+	case StraightLineHorizontalUp:
+		ArrayInput = LineUp();
+		break;
+	case StraightLineHorizontalDown:
+		ArrayInput = LineDown();
+		break;
+	}
+	
+}
+
+void APC_Main::CreateFormationArray(FVector input)
+{
+	FormationVector.AddUnique(input);
+	NewPosition.Set(input.X, input.Y, input.Z);
+}
+
+FVector APC_Main::LineLeft()
+{
+	return FVector(NewPosition.X, NewPosition.Y - 150.f, NewPosition.Z);
+}
+
+FVector APC_Main::LineRight()
+{
+	return FVector(NewPosition.X, NewPosition.Y + 150.f, NewPosition.Z);
+}
+
+FVector APC_Main::LineUp()
+{
+	return FVector(NewPosition.X + 150, NewPosition.Y, NewPosition.Z);
+}
+
+FVector APC_Main::LineDown()
+{
+	return FVector(NewPosition.X - 150, NewPosition.Y, NewPosition.Z);
+}
+
 
--- RTSProject/Source/RTSProject/PC_Main.h
+++ RTSProject/Source/RTSProject/PC_Main.h
@@ -5,13 +5,53 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "PC_Main.generated.h"
-
 /**
- * 
+ *
  */
+UENUM(BlueprintType)
+enum FormationType
+{
+	StraightLineVerticalRight,
+	StraightLineVerticalLeft,
+	StraightLineHorizontalUp,
+	StraightLineHorizontalDown,
+	Stairs,
+	Square
+};
 UCLASS()
 class RTSPROJECT_API APC_Main : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Formation")
+		void CallFormation();
+	UFUNCTION(BlueprintCallable, Category = "Formation")
+		void MakeFormation(FVector rightClick, TArray<AActor*> units);
+
+	UPROPERTY(BlueprintReadWrite, Category = Position)
+		TArray<FVector> FormationVector;
+
+	//UPROPERTY(BlueprintReadWrite, Category = Unit)
+	//	TArray<AActor*> Units;
+
+	UPROPERTY(BlueprintReadWrite, Category = Position)
+		FVector NewPosition;
+
+	UPROPERTY(BlueprintReadWrite, Category = Position)
+		TEnumAsByte<FormationType> formationType;
+
+	UFUNCTION(BlueprintCallable, Category = "Formation")
+		void CreateFormationArray(FVector input);
+private:
+
+	FVector ArrayInput;
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		FVector LineLeft();
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		FVector LineRight();
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		FVector LineUp();
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		FVector LineDown();
 };
